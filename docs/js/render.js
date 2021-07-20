8b3596aa2793490bdeb2a85fc4862892e6bc450d
Boaz Yoo <blazeryoo1921@gmail.com>
2021-07-20 19:30:24 +0000

    docs/ for github project page

---
+++ docs/js/render.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("#wallpaper").load("elements/wallpaper.html");
+  $("#shelf").load("elements/shelf.html");
+  $("#launcher").load("elements/launcher.html");
+  $("#menu-toggle").load("elements/menu-toggle.html");
+});
