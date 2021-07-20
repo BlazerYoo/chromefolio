0b8929dc9ac9f36a612511090d93b3fa7237dab6
Boaz Yoo <blazeryoo1921@gmail.com>
2021-07-20 19:34:57 +0000

    remove docs (updated pages from docs/ -> (root)/)

--- docs/js/render.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $("#wallpaper").load("elements/wallpaper.html");
-  $("#shelf").load("elements/shelf.html");
-  $("#launcher").load("elements/launcher.html");
-  $("#menu-toggle").load("elements/menu-toggle.html");
-});
--- docs/js/time.js
+++
@@ -1,9 +0,0 @@
-(function startTime() {
-  let time = new Date().toLocaleTimeString(navigator.language, {hour: "2-digit", minute:"2-digit"});
-  if (time[0] === "0") {
-    document.getElementById("time").innerHTML = time.substring(1, 5);
-  } else {
-    document.getElementById("time").innerHTML = time.substring(0, 5);
-  }
-  t = setTimeout(function() {startTime()}, 500);
-})();
